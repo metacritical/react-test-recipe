646e4b19ef1f6a714f1a492b8d93e689207c6093
Pankaj Doharey <pankajdoharey@gmail.com>
2019-07-19 15:10:56 +0100

    backtracking support

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ const Ingredient = ({instr, index, setInstr}) => {
       const newInstr = [...instr];
       newInstr[index+1].visibility = true;
       setInstr(newInstr);
+    }else if(!e.target.checked && index > 0 && index+1 < instr.length){
+      const newInstr = [...instr];
+      newInstr[index+1].visibility = false;
+      setInstr(newInstr);
     }
   };
 
@@ -25,14 +29,20 @@ function App() {
   const [instr, setInstr] = useState([
     {desc: "Get The flour", check: false, visibility: true},
     {desc: "Beat the eggs", check: false, visibility: false},
-    {desc: "Bake the cake", check: false, visibility: false}
+    {desc: "Add Sugar", check: false, visibility: false},
+    {desc: "Melt Butter", check: false, visibility: false},
+    {desc: "Add chocolate chips", check: false, visibility: false},
+    {desc: "Add Baking Powder", check: false, visibility: false},
+    {desc: "Mix the Batter", check: false, visibility: false},
+    {desc: "Heat The Oven", check: false, visibility: false},
+    {desc: "Bake the Cake", check: false, visibility: false},
   ]);
 
 
 
   return (
     <div id="cooking-bot">
-      {instr.map((inst, i) => <Ingredient  key={i} setInstr={setInstr} instr={instr} index={i}/> )}
+      {instr.map((_, i) => <Ingredient  key={i} setInstr={setInstr} instr={instr} index={i}/> )}
     </div>
   );
 }
